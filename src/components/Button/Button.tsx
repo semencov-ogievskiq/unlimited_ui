59c305525ce6dc8812a25b079baf482c32e9aaf2
amso <semencov_ogievskiq@mail.ru>
2022-10-02 23:41:10 +0300

    add husky

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -2,7 +2,7 @@ import { FC } from 'react'
 import { StyledButton } from './styles'
 import { ButtonProps } from './types'
 
-export const Button: FC<ButtonProps> = (props: ButtonProps) => {
+export const Button: FC<ButtonProps> = (props) => {
   const {
     children
   } = props
