5ef39bc194f04789b561e0a9560a32ece0fb046b
amso <semencov_ogievskiq@mail.ru>
2022-10-03 00:16:34 +0300

    add custom storybook theme

---
+++ .storybook/UnlimitedUITheme.js
@@ -0,0 +1,9 @@
+import { create } from '@storybook/theming';
+
+export default create({
+  base: 'light',
+  brandTitle: 'Unlimited UI',
+  brandUrl: 'https://github.com/semencov-ogievskiq/unlimited_ui',
+  brandImage: 'https://www.downloadclipart.net/medium/unlimited-png-file.png',
+  brandTarget: '_self',
+});
---
+++ .storybook/manager.js
@@ -0,0 +1,6 @@
+import { addons } from '@storybook/addons';
+import UnlimitedUITheme from './UnlimitedUITheme';
+
+addons.setConfig({
+  theme: UnlimitedUITheme,
+});
